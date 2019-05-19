150e9b6526e820731e14f0f7b9c198578f962f51
Андрей <zolotarev@ckr-sota.ru>
2019-05-19 13:52:17 +0300

    правка вызова add argument

--- HotelBot/Logic/Hotels/HotelPriceClient.py
+++ HotelBot/Logic/Hotels/HotelPriceClient.py
@@ -36,7 +36,7 @@ class HotelPriceClient:
         options = Options()
         options.add_argument("--headless");
         options.add_argument('--no-sandbox');
-        options.addArguments("--disable-dev-shm-usage");
+        options.add_argument("--disable-dev-shm-usage");
         driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver');
         driver.get('https://www.skyscanner.ru/hotels');
 
