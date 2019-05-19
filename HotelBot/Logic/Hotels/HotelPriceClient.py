f1a16130c5c701b0a48dbd93d175cdf667758f20
Андрей <zolotarev@ckr-sota.ru>
2019-05-19 13:50:06 +0300

    Доработки

--- HotelBot/Logic/Hotels/HotelPriceClient.py
+++ HotelBot/Logic/Hotels/HotelPriceClient.py
@@ -34,9 +34,10 @@ class HotelPriceClient:
 
     def getElement(self):
         options = Options()
-        #options.add_argument("--headless");
-        #options.add_argument('--no-sandbox');
-        driver = webdriver.Chrome(chrome_options=options, executable_path='./chromedriver');
+        options.add_argument("--headless");
+        options.add_argument('--no-sandbox');
+        options.addArguments("--disable-dev-shm-usage");
+        driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver');
         driver.get('https://www.skyscanner.ru/hotels');
 
         delay = 45;
